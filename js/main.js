bc452633bae45e247870624cff43fc8657705e0a
Anastasiia <105418918+Tarasova09@users.noreply.github.com>
2022-05-25 16:39:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.menu-open').click(function () {
+		$('.menu').toggleClass('show-menu')
+	})
+});
+
+$(function () {
+	$('.menu-open-footer').click(function () {
+		$('.footer-menu').toggleClass('show-menu')
+	})
+});
